6262e88f66c4f53c1c00777210441897ee0e8bf1
lina-22 <haquefahinur@gmail.com>
2023-11-15 21:51:15 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,4 +42,6 @@ export class AppComponent {
   }
 
   //http call
+  imageurl =
+    'https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg';
 }
